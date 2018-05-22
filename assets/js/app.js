030cc426ab9b28b35b8fa41d161761a9c15a1bd2
Cindy Lopez <cindy.lopezoportus@gmail.com>
2018-05-22 11:53:04 -0400

    subiendo el ejercicio de prueba Fork

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    var btn = document.getElementById("button-change");
+    var txt = document.getElementById("paragraph");
+    var btnDos = document.getElementById("magic");
+    var txtDos = document.getElementById("text");
+
+    btn.addEventListener("click", function() {
+        txt.innerHTML = "<h2>Git y Github TE AMAMOS</h2>"
+    })
+
+    btnDos.addEventListener("click", function(){
+        txtDos.classList.add("disappear");
+    })
+
+}
